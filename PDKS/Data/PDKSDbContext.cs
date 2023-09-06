8d804044da7f227fd423f6a1066c338ac99b213d
misagul <isagul7890@gmail.com>
2023-09-06 09:21:20 +0300

    Add project files.

---
+++ PDKS/Data/PDKSDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PDKS.Models;
+
+namespace PDKS.Data
+{
+    public class PDKSDbContext : DbContext
+    {
+        public PDKSDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
